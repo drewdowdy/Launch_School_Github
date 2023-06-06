ec0cec7cbe1bde4a166cc97fb63694d6f6dc49e2
Drew D <drewdowdy109@gmail.com>
2023-06-06 14:08:24 -0400

    added basics 5 & 6 files

---
+++ basics_exercises/basics6.rb
@@ -0,0 +1,11 @@
+#Exercise 6; write a program that calculates the squares of 3 float numbers and outputs results
+
+num1 = 5
+num2 = 6
+num3 = 7
+
+print "#{num1} squared is "
+puts num1 * num1
+print "#{num2} squared is "
+puts num2 * num2
+print "#{num3} squared is "
