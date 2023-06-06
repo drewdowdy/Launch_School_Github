966aa0e8bfe5e1a8e7420bcf3742b1906c47dfb4
Drew D <drewdowdy109@gmail.com>
2023-06-06 13:27:25 -0400

    put user name 10 times

--- variables_exercises/name.rb
+++ variables_exercises/name.rb
@@ -1,5 +1,12 @@
 #Exercise 1: ask the user to type in name and print greeting
+#Exercise 3: print user name 10 times
 
 puts "What's your name? "
 name = gets.chomp
 puts "Greetings #{name}! You're using Ruby :)"
+10.times { puts name }
+#used curly brakets becuase it is one line of code
+
+#10.times do
+#   puts name
+#end
