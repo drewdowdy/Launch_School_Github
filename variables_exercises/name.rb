c45ebd854c13fba4855d042934aefe726a9669ef
Drew D <drewdowdy109@gmail.com>
2023-06-06 13:36:00 -0400

    displays user's full name and ten times

--- variables_exercises/name.rb
+++ variables_exercises/name.rb
@@ -1,10 +1,13 @@
 #Exercise 1: ask the user to type in name and print greeting
 #Exercise 3: print user name 10 times
+#Exercise 4: get user first & last name and display together
 
-puts "What's your name? "
-name = gets.chomp
-puts "Greetings #{name}! You're using Ruby :)"
-10.times { puts name }
+puts "What's your first name? "
+firstname = gets.chomp
+puts "What's your last name? "
+lastname = gets.chomp
+puts "Your full name is #{firstname} #{lastname}!"
+10.times { puts firstname + " " + lastname }
 #used curly brakets becuase it is one line of code
 
 #10.times do
