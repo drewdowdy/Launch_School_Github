3845004f524d2f7c1b23c9bdeb7011b75bd272ed
Drew D <drewdowdy109@gmail.com>
2023-06-12 17:05:56 -0400

    added greeting.rb and evaluate.rbto file

---
+++ methods_exercises/greeting.rb
@@ -0,0 +1,8 @@
+#Ex1: Print a greeting message
+#should have a method called "greeting" that takes a "name" and returns a string
+
+def greeting(name)
+    "Well hi there #{name}! How's it going?"
+end
+
+puts greeting("Drew")
