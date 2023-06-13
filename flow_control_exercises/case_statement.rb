ad36d69a2ccc6d9864f989580287a6f7f499f751
Drew D <drewdowdy109@gmail.com>
2023-06-13 11:12:37 -0400

    added b section with refactored code

--- flow_control_exercises/case_statement.rb
+++ flow_control_exercises/case_statement.rb
@@ -9,3 +9,19 @@ when 6
 else
     puts "a is neither 5 nor 6"
 end
+
+# refactored (different code, same function)
+
+puts "What number is b?"
+b = gets.chomp.to_i
+
+answer = case b
+when 5
+    "b is 5"
+when 6
+    "b is 6"
+else
+    "b is neither 5 nor 6"
+end
+
+puts answer
