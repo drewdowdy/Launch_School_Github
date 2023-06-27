4da83ad4abb1b719ed2eaec71ad1ca5afbca1b21
Drew D <drewdowdy109@gmail.com>
2023-06-27 11:50:44 -0400

    saved file

--- hashes/iterating_over_hashes.rb
+++ hashes/iterating_over_hashes.rb
@@ -1,2 +1,2 @@
 person = {name: "Billy", height: "190cm", weight: "100kg", hair: "dark brown"}
-person.each {|key,val| puts "His #{key} is #{val}!"}
+person.each {|key,val| puts "His #{key} is #{val}!"}ti
