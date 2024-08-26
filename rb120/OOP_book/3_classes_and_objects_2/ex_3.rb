227ffb57f811f3a39a2d3961f6c14a6756e837bb
Drew D <drewdowdy109@gmail.com>
2024-08-26 17:19:35 -0400

    created ex_3

---
+++ rb120/OOP_book/3_classes_and_objects_2/ex_3.rb
@@ -0,0 +1,26 @@
+class Person
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new("Steve")
+bob.name = "Bob"
+
+# test.rb:9:in `<main>': undefined method `name=' for
+  #<Person:0x007fef41838a28 @name="Steve"> (NoMethodError)
+
+# We need to change `attr_reader` to `attr_accessor` in order to make getter and setter methods for `:name`
+
+class Person
+  attr_accessor :name # now it has getter and setter methods
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new("Steve")
+bob.name = "Bob"
