276b48339aa601fc10494980bbcccdef56a2b5c9
Drew D <drewdowdy109@gmail.com>
2024-08-26 16:01:12 -0400

    updated HumanBeing and Speak classes

--- rb120/OOP_book/1_the_object_model/good_dog.rb
+++ rb120/OOP_book/1_the_object_model/good_dog.rb
@@ -2,6 +2,10 @@ module Speak
   def speak(sound)
     puts sound
   end
+
+  def yell(sound)
+    puts sound.upcase
+  end
 end
 
 module Information
@@ -30,10 +34,15 @@ end
 class HumanBeing
   attr_accessor :name, :height, :weight
   include Speak, Information
+
+  def whisper(sound)
+    puts "Psst, #{sound.downcase}"
+  end
 end
 
 sparky = GoodDog.new('Sparky', '13in', '20lbs')
 sparky.speak("Arf!") # => Arf!
+sparky.yell('Woof!')
 sparky.info
 puts ''
 puts "---GoodDog ancestors---"
@@ -43,6 +52,8 @@ puts ''
 
 bob = HumanBeing.new('Bob', '6ft 2in', '145lbs')
 bob.speak('Hello!')  # => Hello!
+bob.yell('Get back here!')
+bob.whisper('WHAT THE HECK?')
 bob.info
 puts ''
 puts "---HumanBeing ancestors---"
