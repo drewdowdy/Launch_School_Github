26d904e29302a2933c5be365c6bc6c600af66be7
Drew D <drewdowdy109@gmail.com>
2024-08-28 13:29:43 -0400

    created exercises and find_the_class

---
+++ rb120/exercises/OO_Basics_Objects_and_Classes_1/find_the_class.rb
@@ -0,0 +1,14 @@
+# Update the following code so that, instead of printing the values, each statement prints the name of the class to which it belongs.
+
+puts "Hello"
+puts 5
+puts [1, 2, 3]
+
+# Exptected outputs:
+# String
+# Integer
+# Array
+
+puts "Hello".class
+puts 5.class
+puts [1, 2, 3].class
