9b292247a727b5181514cb11bf27806687028271
Drew D <drewdowdy109@gmail.com>
2024-08-29 13:23:09 -0400

    created 5_towable_1

---
+++ rb120/exercises/OO_Basics_Inheritance/5_towable_1.rb
@@ -0,0 +1,15 @@
+module Towable    # create `Towable` module
+  def tow         # define method `#tow`
+    puts 'I can tow a trailer!'
+  end
+end
+
+class Truck
+  include Towable # mix `Towable` module into `Truck` class with `include` method
+end
+
+class Car
+end
+
+truck1 = Truck.new
+puts truck1.tow # => 'I can tow a trailer!'
