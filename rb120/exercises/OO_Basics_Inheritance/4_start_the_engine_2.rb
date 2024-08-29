1d1cf55fec0d74af358b0206ffb6785d851db7ed
Drew D <drewdowdy109@gmail.com>
2024-08-29 12:06:53 -0400

    created 4_start_the_engine_2

---
+++ rb120/exercises/OO_Basics_Inheritance/4_start_the_engine_2.rb
@@ -0,0 +1,14 @@
+class Vehicle
+  def start_engine
+    'Ready to go!'
+  end
+end
+
+class Truck < Vehicle
+  def start_engine(speed)
+    super() + ' ' + "Drive #{speed} please!" # `#super` with empty parentheses passes no arguments
+  end
+end
+
+truck1 = Truck.new              
+puts truck1.start_engine('fast') # Ready to go! Drive fast, please!
