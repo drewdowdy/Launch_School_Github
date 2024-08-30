4aaca84902c74b1b9da8c1de40732aeccd80a7ac
Drew D <drewdowdy109@gmail.com>
2024-08-30 11:54:20 -0400

    updated wedding

--- rb120/lesson_2/polymorphism_and_encapsulation/wedding.rb
+++ rb120/lesson_2/polymorphism_and_encapsulation/wedding.rb
@@ -42,4 +42,14 @@ george = Chef.new
 linda = Decorator.new
 stacy = Musician.new
 
+Wedding.prepare([george, linda, stacy]) 
 
+=begin
+
+1) `Wedding.prepare` passes array to the #prepare method in `Wedding` where `#each` is invoked on the array
+2) On each iteration of `#each`, `#prepare_wedding` is invoked on each person in the array
+3) Each class of the person has a `#prepare_wedding` method in their classes which invokes their own respective specific preparation method based on the class of the calling object.
+
+This is an example of polymorphic code since each of the people `george`, `linda`, and `stacy` are instances of different classes, but can all be passed to an invocation of #prepare and have their own independent outcomes.
+
+=end
