4cb92fdd68239d3fea7f1729a89ac2907f5962e4
Drew D <drewdowdy109@gmail.com>
2024-09-02 10:37:22 -0400

    created duck_typing

---
+++ rb120/lesson_2/polymorphism_and_encapsulation/duck_typing.rb
@@ -0,0 +1,34 @@
+class Duck
+  def quack
+    puts "Quack!"
+  end
+
+  def feathers
+    puts "The duck has white and gray feathers."
+  end
+end
+
+class Person
+  def quack
+    puts "The person imitates a duck."
+  end
+
+  def feathers
+    puts "The person picks up a feather and shows it."
+  end
+
+  def name
+    puts "John Smith"
+  end
+end
+
+def in_the_forrest(duck)
+  # These methods are duck typing because we don't have to specify which class's #quack or #feathers method we are referring to. Ruby understands that if we call #quack on an object with the ability to use a #quack method, then Ruby can invoke the method.
+  duck.quack 
+  duck.feathers 
+end
+
+donald = Duck.new
+john = Person.new
+
+[donald, john].each { |duck| in_the_forrest(duck) }
