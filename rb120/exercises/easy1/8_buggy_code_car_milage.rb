5fc85ba0092628a08ad52d22b214c77f0f9b7fd4
Drew D <drewdowdy109@gmail.com>
2024-09-04 16:41:30 -0400

    created 8_buggy_code_car_milage

---
+++ rb120/exercises/easy1/8_buggy_code_car_milage.rb
@@ -0,0 +1,42 @@
+# The problem with this code is that in the `#increment_milage` method, the local variable `mileage` is assigned to `total`, but the instance variable `@mileage` is what we want to reassign. We could also call the setter method `#mileage=` on `self`
+
+class Car
+  attr_accessor :mileage
+
+  def initialize
+    @mileage = 0
+  end
+
+  def increment_mileage(miles)
+    total = mileage + miles
+    @mileage = total # update the instance variable
+  end
+
+  def print_mileage
+    puts mileage
+  end
+end
+
+# OR
+
+class Car
+  attr_accessor :mileage
+
+  def initialize
+    @mileage = 0
+  end
+
+  def increment_mileage(miles)
+    total = mileage + miles
+    self.mileage = total
+  end
+
+  def print_mileage
+    puts mileage
+  end
+end
+
+car = Car.new
+car.mileage = 5000
+car.increment_mileage(678)
+car.print_mileage  # => 5678
