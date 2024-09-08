a547295abba6a75a307beeb99a2a83942ed2b381
Drew D <drewdowdy109@gmail.com>
2024-09-08 15:58:45 -0400

    created 4_reverse_engineer

---
+++ rb120/exercises/easy_2/4_reverse_engineer.rb
@@ -0,0 +1,19 @@
+# First, we need to allow objects to be instantiated with letters and let `@letters` reference the input. Then, define a `#uppercase` instance method that can be used outside the class. Finally,  define a `::lowercase` class method that can be invoked on the class `Transform`.
+
+class Transform
+  def initialize(letters)
+    @letters = letters
+  end
+
+  def uppercase
+    @letters.upcase
+  end
+
+  def self.lowercase(letters)
+    letters.downcase
+  end
+end
+
+my_data = Transform.new('abc')
+puts my_data.uppercase          # => ABC
+puts Transform.lowercase('XYZ') # => xyz
