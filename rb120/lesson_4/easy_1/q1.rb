5755aabd31578fb23deb445c196158c498776758
Drew D <drewdowdy109@gmail.com>
2024-09-19 14:56:06 -0400

    created q1

---
+++ rb120/lesson_4/easy_1/q1.rb
@@ -0,0 +1,14 @@
+# Which are objects in ruby?
+
+true
+"hello"
+[1, 2, 3, "happy days"]
+124
+
+=begin
+
+Solution:
+
+All of them are considered objects in Ruby. In Ruby, almost everything is considered an object.
+
+=end
