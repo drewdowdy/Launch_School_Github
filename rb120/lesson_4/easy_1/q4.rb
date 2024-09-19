42260f451ec73b89863f9614349588f903615396
Drew D <drewdowdy109@gmail.com>
2024-09-19 17:54:22 -0400

    created q4

---
+++ rb120/lesson_4/easy_1/q4.rb
@@ -0,0 +1,9 @@
+# How do we create a new instance of this class?
+
+class AngryCat
+  def hiss
+    puts "Hisssss!!!"
+  end
+end
+
+AngryCat.new # We can instantiate an object from the `AngryCat` class with the `#new` class method.
