2c41082d6461d1918d1525fdd7407768e292117f
Drew D <drewdowdy109@gmail.com>
2024-09-20 13:05:54 -0400

    updated q4

--- rb120/lesson_4/easy_2/q4.rb
+++ rb120/lesson_4/easy_2/q4.rb
@@ -1,7 +1,9 @@
 # To simplify this class, we can add a `attr_accessor` for `:type`, replacing the getter and setter methods `#type` and `#type=`.
 
+# We can also replace `@type` with `type` in the invocation of `#describe_type`. Removing the `@` means that the return value of `#type` is used instead of the instance variable `@type` directly. It is considered cleaner and standard practice.
+
 class BeesWax
-  attr_accessor :type
+  attr_accessor :type # replaces `#type` and `#type=`
 
   def initialize(type)
     @type = type
@@ -16,7 +18,8 @@ class BeesWax
   # end
 
   def describe_type
-    puts "I am a #{@type} of Bees Wax"
+    # puts "I am a #{@type} of Bees Wax"
+    puts "I am a #{type} of Bees Wax" # use `#type`, not `@type`
   end
 end
 
