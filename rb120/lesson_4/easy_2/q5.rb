d47c9cc7c36e080e3afec44e405d67fe116fa555
Drew D <drewdowdy109@gmail.com>
2024-09-20 13:12:49 -0400

    updated q5

--- rb120/lesson_4/easy_2/q5.rb
+++ rb120/lesson_4/easy_2/q5.rb
@@ -4,10 +4,10 @@ excited_dog = "excited dog"   # local variable
 
 =begin
 
-Variable Nomenclature      Scope
--------- ----------------- ------------------------------
-Local    n/a               Depends on initialization spot
-Instance preceded by `@`   Object level
-Class    preceded by `@@`  Class level
+Variable Nomenclature     Scope
+-------- ---------------- ------------------------------
+Local    n/a              Depends on initialization spot
+Instance preceded by `@`  Object level
+Class    preceded by `@@` Class level
 
 =end
