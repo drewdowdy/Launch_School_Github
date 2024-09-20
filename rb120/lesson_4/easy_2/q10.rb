37acac850a3458ac0b27d24509a021ffaabbccfa
Drew D <drewdowdy109@gmail.com>
2024-09-20 14:41:09 -0400

    created q10

---
+++ rb120/lesson_4/easy_2/q10.rb
@@ -0,0 +1,10 @@
+=begin
+
+Benefits of OOP
+
+- Relationships between objects are clearer.
+- Allows programmers to abstract their ideas.
+- Prevents 'spaghetti code' and allows programmers to change any one part of a code without it affecting other parts
+- Prevents unecessary repetition and keeps code DRY. (DRY - Don't Repeat Yourself)
+
+=end
