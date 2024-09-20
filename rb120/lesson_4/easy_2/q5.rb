a1a47b5fd78d8b48737f5848a9eac6d78c3c8119
Drew D <drewdowdy109@gmail.com>
2024-09-20 13:10:39 -0400

    created q5

---
+++ rb120/lesson_4/easy_2/q5.rb
@@ -0,0 +1,13 @@
+excited_dog = "excited dog"   # local variable
+@excited_dog = "excited dog"  # instance variable
+@@excited_dog = "excited dog" # class variable
+
+=begin
+
+Variable Nomenclature      Scope
+-------- ----------------- ------------------------------
+Local    n/a               Depends on initialization spot
+Instance preceded by `@`   Object level
+Class    preceded by `@@`  Class level
+
+=end
