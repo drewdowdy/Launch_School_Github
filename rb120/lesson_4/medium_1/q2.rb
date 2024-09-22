5736b932d60b69c0bfe376dc0368cc1f54ca0fd6
Drew D <drewdowdy109@gmail.com>
2024-09-22 12:59:47 -0400

    created q2

---
+++ rb120/lesson_4/medium_1/q2.rb
@@ -0,0 +1,15 @@
+class InvoiceEntry
+  attr_reader :quantity, :product_name
+
+  def initialize(product_name, number_purchased)
+    @quantity = number_purchased
+    @product_name = product_name
+  end
+
+  def update_quantity(updated_count)
+    # prevent negative quantities from being set
+    quantity = updated_count if updated_count >= 0
+  end
+end
+
+# Error: there is only an `attr_reader` for `:quantity` so using a setter method `#quantity=` will not work. We can fix this by either accessing the instance variable `@quantity` directly, or create an `attr_writer` for `:quantity`
