28c51ec8349a001c14b69625b6c5b39690801c4a
Drew D <drewdowdy109@gmail.com>
2024-09-23 12:29:39 -0400

    created q3

--- rb120/lesson_4/hard_1/q2.rb
+++ rb120/lesson_4/hard_1/q2.rb
@@ -1,4 +1,4 @@
-# Create a `Motorboat` class
+# Seafaring vehicles have an additional 10km of range. Make sure this is accounted for.
 
 module Rangable
   attr_accessor :speed, :heading
@@ -39,22 +39,27 @@ class Motorcycle < WheeledVehicle
   end
 end
 
-class Catamaran
+class SeaVehicle
   attr_reader :propeller_count, :hull_count
   
   include Rangable
-
+  
   def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
     @num_propellers = num_propellers
     @num_hulls = num_hulls
     self.fuel_efficiency = km_traveled_per_liter
     self.fuel_capacity = liters_of_fuel_capacity
   end
+
+  def range
+    super + 10
+  end
+end
+
+class Catamaran < SeaVehicle
 end
   
-class Motorboat < Catamaran
-  # single propeller and hull, similar to catamaran
-
+class Motorboat < SeaVehicle
   def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
     super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
   end
