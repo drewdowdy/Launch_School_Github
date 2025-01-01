d29756d5a8303e37fee08174200d29f02ba0e12c
Drew D <drewdowdy109@gmail.com>
2025-01-01 17:57:22 -0500

    updated names to car and car_test

--- rb130/lesson_2/car_test/car_test.rb
+++ rb130/lesson_2/car_test/car_test.rb
@@ -15,4 +15,33 @@ class CarTest < Minitest::Test
     car = Car.new
     assert_equal(3, car.wheels)
   end
+
+  def test_car_exists
+    car = Car.new
+    assert(car)
+  end
+
+  def test_name_is_nil
+    car = Car.new
+    assert_nil(car.name)
+  end
+
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")   # this code raises ArgumentError, so this assertion passes
+    end
+  end
+
+  def test_instance_of_car
+    car = Car.new
+    assert_instance_of(Car, car)
+  end
+
+  def test_includes_car 
+    car = Car.new
+    arr = [1, 2, 3]
+    arr << car
+  
+    assert_includes(arr, car) # `assert_includes` invokes `assert` twice behind the scenes (+2 assertions)
+  end
 end
