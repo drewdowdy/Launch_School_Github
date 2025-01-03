d3cbbc38cfcd89e5b9b2be82298f49b1afa3ba12
Drew D <drewdowdy109@gmail.com>
2025-01-03 06:36:18 -0500

    further exploration

--- rb130/exercises/2_optional_blocks.rb
+++ rb130/exercises/2_optional_blocks.rb
@@ -5,3 +5,13 @@ end
 p compute { 5 + 3 } == 8
 p compute { 'a' + 'b' } == 'ab'
 p compute == 'Does not compute.'
+
+# Further Exploration
+
+def compute_2(argument)
+  block_given? ? yield(argument) : 'Does not compute.'
+end
+
+p compute_2(3) { |num| num + 4 } == 7
+p compute_2('a') { |letter| letter + 'cdc'} == 'acdc'
+p compute_2('hello') == 'Does not compute.'
