0f9097cf2048f186430bcdac00da396393284c4b
Drew D <drewdowdy109@gmail.com>
2025-01-03 17:54:32 -0500

    finished 8_iterators_true_for_none

--- rb130/exercises/8_iterators_true_for_none.rb
+++ rb130/exercises/8_iterators_true_for_none.rb
@@ -26,3 +26,23 @@ p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
 p none?([1, 3, 5, 7]) { |value| true } == false
 p none?([1, 3, 5, 7]) { |value| false } == true
 p none?([]) { |value| true } == true
+
+# Alternate Solution
+
+def any?(array)
+  array.each { |e| return true if yield(e) }
+  false
+end
+
+def none?(array)
+  !any?(array) { |e| yield(e) }
+end
+
+puts ''
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true
