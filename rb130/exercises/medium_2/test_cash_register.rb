221a53346ac96b8eb72b94c3c1d3d8f775e459b3
Drew D <drewdowdy109@gmail.com>
2025-01-10 17:40:39 -0500

    renamed test class

--- rb130/exercises/medium_2/test_cash_register.rb
+++ rb130/exercises/medium_2/test_cash_register.rb
@@ -5,24 +5,6 @@ Minitest::Reporters.use!
 require_relative 'cash_register'
 require_relative 'transaction'
 
-class TestCashRegister < Minitest::Test
-  def setup
+class CashRegisterTest < Minitest::Test
 
-  end
-
-  def test_change(transaction)
-
-  end
-
-  def test_give_receipt(transaction)
-
-  end
-
-  def test_accept_money(transaction)
-
-  end
-
-  def test_start_transaction(transaction)
-
-  end
 end
