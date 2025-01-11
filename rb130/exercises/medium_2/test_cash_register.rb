942bad7212c6ec02072b684520da03101cb75560
Drew D <drewdowdy109@gmail.com>
2025-01-11 13:18:43 -0500

    finished test_change

--- rb130/exercises/medium_2/test_cash_register.rb
+++ rb130/exercises/medium_2/test_cash_register.rb
@@ -9,9 +9,18 @@ class CashRegisterTest < Minitest::Test
   def test_accept_money
     @cash_register = CashRegister.new(100)
     @transaction = Transaction.new(25)
-    @transaction.amount_paid = 5
+    @transaction.amount_paid = 30
 
     result = @cash_register.accept_money(@transaction)
-    assert_equal(105, result)
+    assert_equal(130, result)
+  end
+
+  def test_change
+    @cash_register = CashRegister.new(100)
+    @transaction = Transaction.new(25)
+    @transaction.amount_paid = 30
+
+    result = @cash_register.change(@transaction)
+    assert_equal(5, result)
   end
 end
