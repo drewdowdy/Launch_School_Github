e3bcc5e46c367cf1818b5a3637e6c4eff602ef9f
Drew D <drewdowdy109@gmail.com>
2025-01-11 09:16:31 -0500

    finished test_cash_register

--- rb130/exercises/medium_2/test_cash_register.rb
+++ rb130/exercises/medium_2/test_cash_register.rb
@@ -6,5 +6,12 @@ require_relative 'cash_register'
 require_relative 'transaction'
 
 class CashRegisterTest < Minitest::Test
+  def test_accept_money
+    @cash_register = CashRegister.new(100)
+    @transaction = Transaction.new(25)
+    @transaction.amount_paid = 5
 
+    result = @cash_register.accept_money(@transaction)
+    assert_equal(105, result)
+  end
 end
