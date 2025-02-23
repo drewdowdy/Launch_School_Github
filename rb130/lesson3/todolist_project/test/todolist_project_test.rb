924652fa70b9a431e9718557012e385d743efe10
Drew D <drewdowdy109@gmail.com>
2025-02-23 11:32:42 -0500

    added @due_date tests

--- rb130/lesson3/todolist_project/test/todolist_project_test.rb
+++ rb130/lesson3/todolist_project/test/todolist_project_test.rb
@@ -160,4 +160,14 @@ class TodoListTest < Minitest::Test
     assert_equal(list.title, @list.title)
     assert_equal(list.to_s, @list.select{ |todo| todo.done? }.to_s)
   end
+
+  def test_no_due_date
+    assert_nil(@todo1.due_date)
+  end
+
+  def test_due_date
+    due_date = Date.today + 3
+    @todo2.due_date = due_date
+    assert_equal(due_date, @todo2.due_date)
+  end
 end
