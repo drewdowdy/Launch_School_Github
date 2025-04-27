ae93a93fcd4e3464cd6464975f3853f18fa06929
Drew D <drewdowdy109@gmail.com>
2025-04-27 17:00:21 -0400

    created test_f and updated http_server

---
+++ ls170/bash_basics/test_f.sh
@@ -0,0 +1,6 @@
+if [[ -f "./until_loop.sh" ]]
+then
+  echo "Until loop file exists and is not a directory!"
+else
+  echo "No until loop file here..."
+fi
--- ls170/simple_http_server/http_server.sh
+++ ls170/simple_http_server/http_server.sh
@@ -3,12 +3,32 @@
 function server () {
   while true
   do
-    read method path version
+    message_arr=()
+    check=true
+
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    
     if [[ $method = 'GET' ]]
     then
-      echo 'HTTP/1.1 200 OK'
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c <'./www/'$path)\r\n\r\n"; cat "./www/$path"
+      else
+        echo -ne 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
+      fi
     else
-      echo 'HTTP/1.1 400 Bad Request'
+      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
     fi
   done
 }
@@ -20,4 +40,4 @@ do
   netcat -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
 done
 
-# GET ./www/file_name
+# get request --> GET file_name.html HTTP/1.1
