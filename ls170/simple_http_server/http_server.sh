5f3ebfe2095ae16afd23f5d982b400cc4e04ae66
Drew D <drewdowdy109@gmail.com>
2025-04-27 14:49:36 -0400

    made www

--- ls170/simple_http_server/http_server.sh
+++ ls170/simple_http_server/http_server.sh
@@ -1,31 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 function server () {
   while true
   do
-    message_arr=()
-    check=true
-    while $check
-    do
-      read line
-      message_arr+=($line)
-      if [[ "${#line}" -eq 1 ]]
-      then
-        check=false
-      fi
-    done
-    method=${message_arr[0]}
-    path=${message_arr[1]}
+    read method path version
     if [[ $method = 'GET' ]]
     then
-      if [[ -f "./www/$path" ]]
-      then
-        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c <'./www/'$path)\r\n\r\n"; cat "./www/$path"
-      else
-        echo -ne 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
-      fi
+      echo 'HTTP/1.1 200 OK'
     else
-      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
+      echo 'HTTP/1.1 400 Bad Request'
     fi
   done
 }
@@ -34,5 +17,7 @@ coproc SERVER_PROCESS { server; }
 
 while true
 do
-netcat -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+  netcat -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
 done
+
+# GET ./www/file_name
