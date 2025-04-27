847302259dfcee42142f47e46d0a2247f88ade4b
Drew D <drewdowdy109@gmail.com>
2025-04-27 11:55:56 -0400

    updated http_server

--- ls170/simple_http_server/http_server.sh
+++ ls170/simple_http_server/http_server.sh
@@ -1,37 +1,33 @@
 #!/usr/local/bin/bash
 
-function server() {
-while true
-do
-  message_arr=()
-  check=true
-
-  while $check
+function server () {
+  while true
   do
-    read line
-    message_arr+=($line)
-    if [[ "${#line}" -eq 1 ]]
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    if [[ $method = 'GET' ]]
     then
-      check=false
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c <'./www/'$path)\r\n\r\n"; cat "./www/$path"
+      else
+        echo -ne 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
+      fi
+    else
+      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
     fi
   done
-
-  method=${message_arr[0]}
-  path=${message_arr[1]}
-
-  read method path version
-  if [[ $method = 'GET' ]]
-  then
-    if [[ -f "./www/$path" ]]
-    then
-      echo -ne 'HTTP/1.1 200 OK\r\n\r\n';cat "./www/$path"
-    else
-      echo -ne 'HTTP/1.1 404 Not Found\r\n\r\n'
-    fi
-  else
-    echo 'HTTP1.1 400 Bad request';echo\r\n\r\n
-  fi
-done
 }
 
 coproc SERVER_PROCESS { server; }
