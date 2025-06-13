d585df7c504dbe7138d9099132de64d8a00de476
Drew D <drewdowdy109@gmail.com>
2025-06-13 12:22:34 -0400

    updated server to counter

--- rb175/lesson_2/server.rb
+++ rb175/lesson_2/server.rb
@@ -6,7 +6,7 @@ def parse_request(request_line)
   http_method, path_and_params, http = request_line.split(' ')
   path, params = path_and_params.split('?')
 
-  params = params.split('&').each_with_object({}) do |pair, hash|
+  params = (params || '').split('&').each_with_object({}) do |pair, hash|
     key, value = pair.split('=')
     hash[key] = value
   end
@@ -18,34 +18,34 @@ loop do
   client = server.accept
   request_line = client.gets
   
-  next if !request_line.include?('?')
+  next if !request_line || request_line =~ /favicon/ || request_line =~ /devtools/
   
   puts request_line
   
   http_method, path, params = parse_request(request_line)
 
-  client.puts 'HTTP/1.1 200 OK'
-  client.puts 'Content-Type: text/html'
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/html"
   client.puts
-  client.puts '<html>'
-  client.puts '<body>'
-  client.puts '<pre>'
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
   client.puts http_method
   client.puts path
   client.puts params
-  client.puts '</pre>'
-  client.puts '<h1>Rolls!</h1>'
-
-  rolls = params["rolls"].to_i
-  sides = params["sides"].to_i
+  client.puts "</pre>"
   
-  rolls.times do 
-    roll = rand(sides) + 1
-    client.puts '<p>', roll, '</p>'
-  end
+  client.puts "<h1>Counter</h1>"
 
-  client.puts '</body>'
-  client.puts '</html>'
+  number = params["number"].to_i
+
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
+
+  client.puts "</body>"
+  client.puts "</html>"
 
   client.close
 end
