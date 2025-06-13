bdd1483fa2d989ef9ffbb0108a4fc8c37280e3e9
Drew D <drewdowdy109@gmail.com>
2025-06-13 07:57:59 -0400

    created server

---
+++ rb175/lesson_2/server.rb
@@ -0,0 +1,30 @@
+require "socket"
+
+server = TCPServer.new("localhost", 3003)
+
+loop do 
+  client = server.accept
+
+  request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
+  puts request_line
+
+  http_method, path_and_params, http = request_line.split(' ')
+  path, params = path_and_params.split('?')
+
+  params = params.split('&').each_with_object({}) do |pair, hash|
+    key, value = pair.split('=')
+    hash[key] = value
+  end
+
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/plain\r\n\r\n"
+  client.puts request_line
+
+  client.puts request_line
+  client.puts http_method
+  client.puts path
+  client.puts params
+
+  client.close
+end
