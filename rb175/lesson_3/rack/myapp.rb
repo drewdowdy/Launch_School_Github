c1feb48ca3efce8952b8feac073618b174b08281
Drew D <drewdowdy109@gmail.com>
2025-06-13 16:10:20 -0400

    added config

--- rb175/lesson_3/rack/myapp.rb
+++ rb175/lesson_3/rack/myapp.rb
@@ -8,4 +8,20 @@ class MyApp
   end
 end
 
-Rackup::Handler::WEBrick.run MyApp.new
+class FriendlyGreeting
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    body = @app.call(env).last
+
+    [
+      '200',
+      {"Content-Type" => "text/html"},
+      body.prepend("<p>A warm welcome to you!</p>\n")
+    ]
+  end
+end
+
+Rackup::Handler::WEBrick.run FriendlyGreeting.new(MyApp.new)
