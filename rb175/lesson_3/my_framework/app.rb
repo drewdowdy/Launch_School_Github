62d9a05d946ebd4f9685d5ce757f0bfaf1c0ea12
Drew D <drewdowdy109@gmail.com>
2025-06-14 13:14:09 -0400

    refactored #call; added #response

--- rb175/lesson_3/my_framework/app.rb
+++ rb175/lesson_3/my_framework/app.rb
@@ -4,20 +4,27 @@ class App
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      [200, {"Content-Type" => 'text/html'}, [erb(:index)]]
+      status = '200'
+      headers = {"Content-Type" => 'text/html'}
+
+      response(status, headers) do
+        erb :index
+      end
     when '/advice'
       piece_of_advice = Advice.new.generate
-      [
-        200,
-        {"Content-Type" => 'text/html'},
-        [erb(:advice, message: piece_of_advice)]
-      ]
+      status = '200'
+      headers = {"Content-Type" => 'text/html'}
+
+      response(status, headers) do
+        erb :advice, message: piece_of_advice
+      end
     else
-      [
-        404,
-        {"Content-Type" => 'text/html', "Content-Length" => '61'},
-        [erb(:not_found)]
-      ]
+      status = '404'
+      headers = {"Content-Type" => 'text/html', "Content-Length" => '61'}
+      
+      response(status, headers) do
+        erb :not_found
+      end
     end
   end
 
@@ -28,5 +35,10 @@ class App
     message = local[:message]
     content = File.read("views/#{filename}.erb")
     ERB.new(content).result(b)
+  end
+
+  def response(status, headers, body = '')
+    body = yield if block_given?
+    [status, headers, [body]]
   end
 end
