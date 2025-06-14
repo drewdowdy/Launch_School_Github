c1bc2e9a1c28d2070b4bbad3dda2100f7e45df3c
Drew D <drewdowdy109@gmail.com>
2025-06-14 13:01:44 -0400

    added more erb files

--- rb175/lesson_3/my_framework/hello_world.rb
+++ rb175/lesson_3/my_framework/hello_world.rb
@@ -5,12 +5,10 @@ class HelloWorld
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      template = File.read('views/index.erb')
-      content = ERB.new(template)
       [
         200,
         {"Content-Type" => 'text/html'},
-        ["<html><body><h2>Hello World!</h2></body></html>"]
+        [erb(:index)]
       ]
     when '/advice'
       piece_of_advice = Advice.new.generate
@@ -26,5 +24,10 @@ class HelloWorld
         ["<html><body><h4>404 Not Found</h4></body></html>"]
       ]
     end
+  end
+
+  def erb(filename)
+    content = File.read("views/#{filename}.erb")
+    ERB.new(content).result
   end
 end
