721dd27c4538e9149d0054ae12bbfef8c38f025f
Drew D <drewdowdy109@gmail.com>
2025-06-16 17:37:30 -0400

    all files can display

--- rb175/lesson_3/challenges/dynamic_directory_index/list_app.rb
+++ rb175/lesson_3/challenges/dynamic_directory_index/list_app.rb
@@ -4,12 +4,43 @@ require "sinatra/reloader"
 
 get '/' do 
   @docs = Dir.glob('public/*.txt').map! do |doc|
-    File.basename("#{doc}", '.txt')
+    File.basename(doc, '.txt')
   end
 
   erb :home
 end
 
 get '/apple' do 
-  
+  @title = "Apple"
+  @content = File.readlines('public/apple.txt')
+
+  erb :fruit
+end
+
+get '/banana' do 
+  @title = "Banana"
+  @content = File.readlines('public/banana.txt')
+
+  erb :fruit
+end
+
+get '/cantaloupe' do 
+  @title = "Cantaloupe"
+  @content = File.readlines('public/cantaloupe.txt')
+
+  erb :fruit
+end
+
+get '/durian' do 
+  @title = "Durian"
+  @content = File.readlines('public/durian.txt')
+
+  erb :fruit
+end
+
+get '/elderberry' do 
+  @title = "Elderberry"
+  @content = File.readlines('public/elderberry.txt')
+
+  erb :fruit
 end
