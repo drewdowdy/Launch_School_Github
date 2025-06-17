42de417c20df8b1faa10f8a2288bca6ae76d6952
Drew D <drewdowdy109@gmail.com>
2025-06-17 15:36:40 -0400

    updated search; added comments

--- rb175/lesson_3/book_viewer/book_viewer.rb
+++ rb175/lesson_3/book_viewer/book_viewer.rb
@@ -2,10 +2,12 @@ require 'tilt/erubi'
 require "sinatra"
 require "sinatra/reloader"
 
+# `before` is used to pre-load any content that will be globally used by the app
 before do
   @contents = File.readlines("data/toc.txt")
 end
 
+# `helpers` allows for methods to be called in view templates (.erb files)
 helpers do
 
   def in_paragraphs(text)
@@ -16,6 +18,7 @@ helpers do
 
 end
 
+# `get` defines a route in the application; what action to take when a user types in the URL
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
 
@@ -34,10 +37,34 @@ get "/chapters/:number" do
   erb :chapter
 end
 
+# Calls the block for each chapter, passing that chapter's number, name, and contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << {number: number, name: name} if contents.include?(query)
+  end
+
+  results
+end
+
 get "/search" do
+  @results = chapters_matching(params[:query])
   erb :search
 end
 
+# `not_found` route will execute when the app can't find any other route to match a request
 not_found do
   redirect "/"
 end
