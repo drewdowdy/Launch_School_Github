c30f18fdf3632e15c2a1f36f1a791f63222ecd6f
Drew D <drewdowdy109@gmail.com>
2025-06-17 08:49:04 -0400

    print each line

--- rb175/lesson_3/book_viewer/book_viewer.rb
+++ rb175/lesson_3/book_viewer/book_viewer.rb
@@ -12,7 +12,7 @@ end
 get "/chapters/1" do
   @title = "Chapter 1"
   @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+  @chapter = File.readlines("data/chp1.txt")
 
   erb :chapter
 end
