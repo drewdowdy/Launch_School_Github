7374b8fd349717b5efe5de2c49caedf718b0aa87
Drew D <drewdowdy109@gmail.com>
2025-06-18 15:31:45 -0400

    finalized

--- rb175/lesson_3/challenges/users_and_interests/app.rb
+++ rb175/lesson_3/challenges/users_and_interests/app.rb
@@ -6,7 +6,7 @@ On each user's page, display their email address. Also display their interests, 
 + At the bottom of each user's page, list links to all of the other users pages. Do not include the user whose page it is in this list.
 + Add a layout to the application. At the bottom of every page, display a message like this: "There are 3 users with a total of 9 interests."
 + Update the message printed out in #5 to determine the number of users and interests based on the content of the YAML file. Use a view helper method, count_interests, to determine the total number of interests across all users.
-- Add a new user to the users.yaml file. Verify that the site updates accordingly.
++ Add a new user to the users.yaml file. Verify that the site updates accordingly.
 
 =end
 
