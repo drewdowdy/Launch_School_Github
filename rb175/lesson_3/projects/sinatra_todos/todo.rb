997a5c299464b82fac144391fbdd6f2e9e24bc1d
Drew D <drewdowdy109@gmail.com>
2025-06-23 15:21:37 -0400

    added configure, before, & /lists/new

--- rb175/lesson_3/projects/sinatra_todos/todo.rb
+++ rb175/lesson_3/projects/sinatra_todos/todo.rb
@@ -2,16 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
+configure do 
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
-set :session_secret, SecureRandom.hex(32)
+get "/lists/new" do
+  session[:lists] << { name: "New List", todos: [] }
+  redirect "/lists"
+end
