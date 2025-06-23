5bc500a2eb018af158c50c36b2e7655a0c5c32f9
Drew D <drewdowdy109@gmail.com>
2025-06-23 17:04:45 -0400

    refactored

--- rb175/lesson_3/projects/sinatra_todos/todo.rb
+++ rb175/lesson_3/projects/sinatra_todos/todo.rb
@@ -45,7 +45,9 @@ end
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if error = error_for_list_name(list_name)
+  error = error_for_list_name(list_name)
+  
+  if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
