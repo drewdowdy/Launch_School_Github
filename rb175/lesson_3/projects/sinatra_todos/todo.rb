523ec059f40a0ad97e674148a15af9f03622485b
Drew D <drewdowdy109@gmail.com>
2025-06-23 15:42:55 -0400

    added comments

--- rb175/lesson_3/projects/sinatra_todos/todo.rb
+++ rb175/lesson_3/projects/sinatra_todos/todo.rb
@@ -15,15 +15,25 @@ get "/" do
   redirect "/lists"
 end
 
+# GET   /lists      -> view all lists
+# GET   /lists/new  -> new list form
+# POST  /lists      -> create new list
+# GET   /lists/1    -> view a single list
+# GET   /users      -> view all users
+# GET   /users/1    -> view a single user
+
+# View list of lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   session[:lists] << { name: params[:list_name], todos: [] }
   redirect "/lists"
