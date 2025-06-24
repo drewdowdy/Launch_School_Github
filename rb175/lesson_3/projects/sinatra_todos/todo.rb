2400ac75ecc0c5548999454970600808b400d24b
Drew D <drewdowdy109@gmail.com>
2025-06-23 20:32:00 -0400

    can add todos

--- rb175/lesson_3/projects/sinatra_todos/todo.rb
+++ rb175/lesson_3/projects/sinatra_todos/todo.rb
@@ -43,6 +43,13 @@ def error_for_list_name(name)
   end
 end
 
+# Return an error message if the name is invalid. Return nil if name is valid
+def error_for_todo(name)
+  if !(1..100).cover? name.size # best to use #cover; NOT #include (which checks EVERY value)
+    'Todo must be between 1 and 100 characters.'
+  end
+end
+
 # Create a new list
 post '/lists' do
   list_name = params[:list_name].strip
@@ -59,8 +66,9 @@ post '/lists' do
 end
 
 get '/lists/:id' do
-  id = params[:id].to_i
-  @list = session[:lists][id]
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
   erb :list, layout: :layout
 end
 
@@ -68,6 +76,7 @@ end
 get '/lists/:id/edit' do
   id = params[:id].to_i
   @list = session[:lists][id]
+
   erb :edit_list, layout: :layout
 end
 
@@ -91,7 +100,25 @@ end
 # Deletes a todo list
 post '/lists/:id/destroy' do
   id = params[:id].to_i
+
   session[:lists].delete_at(id)
   session[:success] = 'The list has been deleted.'
   redirect '/lists'
+end
+
+# Add a new todo to a list
+post '/lists/:list_id/todos' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+  error = error_for_todo(text)
+
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << { name: text, completed: false }
+    session[:success] = 'The todo was added.'
+    redirect "/lists/#{@list_id}"
+  end
 end
