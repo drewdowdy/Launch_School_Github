5f239776aad8954c843f8c160489ff8c98c14cd1
Drew D <drewdowdy109@gmail.com>
2025-06-29 14:49:05 -0400

    set up configure and requirements

---
+++ rb175/file_based_cms/app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/content_for'
+require 'tilt/erubi'
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+  set :erb, :escape_html => true
+end
+
+get '/' do
+  
+end
