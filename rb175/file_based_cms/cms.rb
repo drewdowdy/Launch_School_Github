8cfa0bfce1e0dbc627e2c548e0e65f34236531fb
Drew D <drewdowdy109@gmail.com>
2025-06-29 16:10:10 -0400

    removed before; added root

--- rb175/file_based_cms/cms.rb
+++ rb175/file_based_cms/cms.rb
@@ -9,12 +9,11 @@ configure do
   set :erb, :escape_html => true
 end
 
-before do
-  @files = Dir.glob('data/*').map do |file|
-    File.basename(file)
-  end
-end
-
 get '/' do
+  root = File.expand_path("..", __FILE__)
+  @files = Dir.glob(root + '/data/*').map do |path|
+    File.basename(path)
+  end
+
   erb :index
 end
