2e9f221afea8c26cfb1017b43b2b518d97442a4b
Drew D <drewdowdy109@gmail.com>
2025-06-29 10:21:48 -0400

    updated for javascripts

--- rb175/lesson_3/sinatra_todos/todo.rb
+++ rb175/lesson_3/sinatra_todos/todo.rb
@@ -139,15 +139,17 @@ post '/lists/:id' do
   end
 end
 
-# Deletes a todo list
-post '/lists/:id/destroy' do
+# Delete a todo list
+post "/lists/:id/destroy" do
   id = params[:id].to_i
-
   session[:lists].delete_at(id)
-  session[:success] = 'The list has been deleted.'
-  redirect '/lists'
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    "/lists"
+  else
+    session[:success] = "The list has been deleted."
+    redirect "/lists"
+  end
 end
-
 # Add a new todo to a list
 post '/lists/:list_id/todos' do
   @list_id = params[:list_id].to_i
@@ -166,14 +168,18 @@ post '/lists/:list_id/todos' do
 end
 
 # Delete a todo from a list
-post '/lists/:list_id/todos/:id/destroy' do
+post "/lists/:list_id/todos/:id/destroy" do
   @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
-  todo_id = params[:id].to_i
 
+  todo_id = params[:id].to_i
   @list[:todos].delete_at todo_id
-  session[:success] = 'The todo has been deleted.'
-  redirect "/lists/#{@list_id}"
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    status 204
+  else
+    session[:success] = "The todo has been deleted."
+    redirect "/lists/#{@list_id}"
+  end
 end
 
 # Update the status of todo
