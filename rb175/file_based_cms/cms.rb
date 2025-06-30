cc154c36f6196b9eb3fab94d20b013093e7f0933
Drew D <drewdowdy109@gmail.com>
2025-06-30 18:30:23 -0400

    updated tests to setup/teardown test directory

--- rb175/file_based_cms/cms.rb
+++ rb175/file_based_cms/cms.rb
@@ -9,46 +9,50 @@ configure do
   set :session_secret, SecureRandom.hex(32)
 end
 
-before do
-  @root = File.expand_path('..', __FILE__)
+# Methods
+def no_file_error(path)
+  "#{File.basename(path)} does not exist." if !File.file?(path)
+end
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
   
-  @files = Dir.glob(@root + '/data/*').map! do |path|
-    File.basename(path)
+  case File.extname(path) # File.extname gets extension name of file
+  when '.txt'
+    headers['Content-Type'] = 'text/plain'
+    content
+  when '.md'
+    render_markdown(content)
   end
 end
 
-helpers do
-  def no_file_error(path)
-    "#{File.basename(path)} does not exist." if !File.file?(path)
-  end
-
-  def render_markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    markdown.render(text)
-  end
-
-  def load_file_content(path)
-    content = File.read(path)
-    
-    case File.extname(path) # gets extension name of file
-    when '.txt'
-      headers['Content-Type'] = 'text/plain'
-      content
-    when '.md'
-      render_markdown(content)
-    end
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
   end
 end
 
 # Shows a list of all files
 get '/' do
+  pattern = File.join(data_path, '*') # File.join will use the right seperator based on OS
+  @files = Dir.glob(pattern).map! do |path|
+    File.basename(path)
+  end
+
   erb :index
 end
 
 # Shows the contents of a file
 get '/:file_name' do
   file_name = params[:file_name]
-  file_path = @root + '/data/' + file_name
+  file_path = File.join(data_path, file_name)
 
   if File.exist?(file_path)
     load_file_content(file_path)
@@ -67,7 +71,7 @@ end
 # Updates the file
 post '/:file_name' do
   file_name = params[:file_name]
-  file_path = @root + '/data/' + file_name
+  file_path = File.join(data_path, file_name)
   user_input = params[:content]
 
   File.write(file_path, user_input)
--- rb175/file_based_cms/test/cms_test.rb
+++ rb175/file_based_cms/test/cms_test.rb
@@ -2,6 +2,7 @@ ENV['RACK_ENV'] = 'test'
 
 require 'minitest/autorun'
 require 'rack/test'
+require "fileutils" # module with tools for Files and Paths
 
 require_relative '../cms.rb'
 
@@ -12,38 +13,59 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
-    get '/'
+    create_document "about.md"
+    create_document "changes.txt"
+
+    get "/"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, 'about.md'
-    assert_includes last_response.body, 'changes.txt'
-    assert_includes last_response.body, 'history.txt'
+    assert_includes last_response.body, "about.md"
+    assert_includes last_response.body, "changes.txt"
   end
 
   def test_view_file
+    create_document 'history.txt', '1993 - Yukihiro Matsumoto dreams up Ruby.'
+
     get '/history.txt'
+
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, '1993 - Yukihiro Matsumoto dreams up Ruby.'
-    assert_includes last_response.body, '2022 - Ruby 3.2 released.'
   end
 
   def test_document_not_found
-    get "/notafile.ext" # Attempt to access a nonexistent file
+    get "/notafile.ext"                                               # Attempt to access a nonexistent file
 
-    assert_equal 302, last_response.status # Assert that the user was redirected
+    assert_equal 302, last_response.status                            # Assert that the user was redirected
 
-    get last_response["Location"] # Request the page that the user was redirected to
+    get last_response["Location"]                                     # Request the page that the user was redirected to
 
     assert_equal 200, last_response.status
     assert_includes last_response.body, "notafile.ext does not exist"
 
-    get "/" # Reload the page
+    get "/"                                                           # Reload the page
     refute_includes last_response.body, "notafile.ext does not exist" # Assert that our message has been removed
   end
 
   def test_viewing_markdown_document
+    create_document 'about.md', "# Ruby is...\n\nA dynamic and open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural and easy to write."
+    
     get "/about.md"
 
     assert_equal 200, last_response.status
@@ -51,7 +73,9 @@ class CMSTest < Minitest::Test
     assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 
-    def test_editing_document
+  def test_editing_document
+    create_document 'changes.txt'
+
     get "/changes.txt/edit"
 
     assert_equal 200, last_response.status
