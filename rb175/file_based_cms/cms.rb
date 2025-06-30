f845f43b42d475f36c71e38d98ef55e43bf63959
Drew D <drewdowdy109@gmail.com>
2025-06-30 09:38:28 -0400

    error for invalid file names

--- rb175/file_based_cms/cms.rb
+++ rb175/file_based_cms/cms.rb
@@ -6,20 +6,34 @@ require 'tilt/erubi'
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
-  set :erb, :escape_html => true
 end
 
-root = File.expand_path('..', __FILE__)
-
-get '/' do
-  @files = Dir.glob(root + '/data/*').map! do |path|
+before do
+  @root = File.expand_path('..', __FILE__)
+  
+  @files = Dir.glob(@root + '/data/*').map! do |path|
     File.basename(path)
   end
+end
+
+get '/' do
   erb :index
 end
 
+def error_for_file_name(file_name)
+  "#{file_name} does not exist." if !@files.include?(file_name) 
+end
+
 get '/:file_name' do
-  file_path = Dir.glob(root + '/data/' + params[:file_name]).first
-  headers['Content-Type'] = 'text/plain'
-  File.read(file_path)
+  file_name = params[:file_name]
+  error = error_for_file_name(file_name)
+
+  if error
+    session[:error] = error
+    redirect '/'
+  else
+    file_path = Dir.glob(@root + '/data/' + file_name).first
+    headers['Content-Type'] = 'text/plain'
+    File.read(file_path)
+  end
 end
--- rb175/file_based_cms/test/cms_test.rb
+++ rb175/file_based_cms/test/cms_test.rb
@@ -28,4 +28,18 @@ class CMSTest < Minitest::Test
     assert_includes last_response.body, '1993 - Yukihiro Matsumoto dreams up Ruby.'
     assert_includes last_response.body, '2022 - Ruby 3.2 released.'
   end
+
+  def test_document_not_found
+  get "/notafile.ext" # Attempt to access a nonexistent file
+
+  assert_equal 302, last_response.status # Assert that the user was redirected
+
+  get last_response["Location"] # Request the page that the user was redirected to
+
+  assert_equal 200, last_response.status
+  assert_includes last_response.body, "notafile.ext does not exist"
+
+  get "/" # Reload the page
+  refute_includes last_response.body, "notafile.ext does not exist" # Assert that our message has been removed
+end
 end
