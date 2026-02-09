3de95d20064e117899c411a7f10f6ac73b0dc1df
Drew D <drewdowdy109@gmail.com>
2026-02-09 09:29:32 -0500

    created 8_arguments_pt_3

--- js210/exercises/2_functions/8_arguments_pt_3.js
+++ js210/exercises/2_functions/8_arguments_pt_3.js
@@ -8,5 +8,11 @@ myValue(a);
 console.log(a);
 
 /*
+Q: What will the following code log to the console and why? Don't run the code until after you have tried to answer.
 
+A: This code will log the following:
+
+> [1, 2, 10]
+
+In this case, when we pass in the array into `myValue()`, both `a` and `b` reference the same array in memory. So when we change the value at index 2, the array referenced by `a` is mutated.
 */
