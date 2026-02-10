a8b662f341e51e8109b5f906ed1f4321c654cf3b
Drew D <drewdowdy109@gmail.com>
2026-02-10 16:52:37 -0500

    added further exploration

--- js210/lesson_3/practice_problems/3_print_multiples.js
+++ js210/lesson_3/practice_problems/3_print_multiples.js
@@ -41,3 +41,38 @@ output (3x and 1x)
 63
 21
 */
+
+/*
+Further exploration:
+
+This solution must step through every number between 100 and 0. Alternatively, you can determine the number's largest multiple that is less than 100, then start the loop with that multiple. With each iteration of the loop, you can decrement the number by the argument; when the result is negative, you can quit.
+
+As above, we must check each multiple to determine whether it is odd. If it is odd, we log it.
+
+Math.floor(100 / number) * number returns the largest multiple of number that is between 0 and 100. If you're not familiar with the floor method, see the documentation.
+*/
+
+function logMultiples(number) {
+  for (let currentMultiple = Math.floor(100 / number) * number; currentMultiple >= number; currentMultiple -= 2) {
+    if (currentMultiple % number === 0) {
+      console.log(currentMultiple); // only prints 99
+    }
+  }
+} 
+
+console.log('Further Exploration');
+
+logMultiples(17);
+/*
+output (5x, 3x and 1x)
+85
+51
+17
+*/
+
+logMultiples(21);
+/*
+output (3x and 1x)
+63
+21
+*/
