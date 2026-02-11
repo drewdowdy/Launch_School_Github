4de241711526eeef9297c414b3f163c4531bbf6c
Drew D <drewdowdy109@gmail.com>
2026-02-11 18:10:10 -0500

    created 14_repeating_strings

---
+++ js210/lesson_3/practice_problems/14_repeating_strings.js
@@ -0,0 +1,28 @@
+/*
+Implement a function that takes a string and a number times as arguments. The function should return the string repeated times number of times. If times is not a number, return undefined. If it is a negative number, return undefined also. If times is 0, return an empty string. You may ignore the possibility that times is a number that isn't an integer.
+*/
+
+function repeat(string, times) {
+  if (times < 0 || typeof(times) !== 'number') {
+    return undefined;
+  } else if (times === 0) {
+    return '';
+  }
+  
+  let result = '';
+
+  for (; times > 0; times -= 1) {
+    result += string;
+  }
+
+  return result;
+}
+
+console.log(repeat('abc', 1));       // "abc"
+console.log(repeat('abc', 2));       // "abcabc"
+console.log(repeat('abc', -1));      // undefined
+console.log(repeat('abc', 0));       // ""
+console.log(repeat('abc', 'a'));     // undefined
+console.log(repeat('abc', false));   // undefined
+console.log(repeat('abc', null));    // undefined
+console.log(repeat('abc', '  '));    // undefined
