dda40854d27f02b4e1605bff614c4062045d88e9
Drew D <drewdowdy109@gmail.com>
2026-02-15 12:09:06 -0500

    completed 6_short_long_short

---
+++ js210/exercises/3_small_problems/6_short_long_short.js
@@ -0,0 +1,22 @@
+/*
+Write a function that takes two strings as arguments, determines the length of the two strings, and then returns the result of concatenating the shorter string, the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
+
+Examples:
+
+shortLongShort('abc', 'defgh');    // "abcdefghabc"
+shortLongShort('abcde', 'fgh');    // "fghabcdefgh"
+shortLongShort('', 'xyz');         // "xyz"
+*/
+
+function shortLongShort(string1, string2) {
+  let first;
+  let second;
+
+  string1.length > string2.length ? [first, second] = [string2, string1] : [first, second] = [string1, string2];
+
+  return first + second + first;
+}
+
+console.log(shortLongShort('abc', 'defgh'));    // "abcdefghabc"
+console.log(shortLongShort('abcde', 'fgh'));    // "fghabcdefgh"
+console.log(shortLongShort('', 'xyz'));         // "xyz"
