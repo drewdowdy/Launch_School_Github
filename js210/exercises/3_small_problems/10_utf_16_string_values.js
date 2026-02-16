0fa9cfd9ac03665ce5c7f6f4f03a80f942491300
Drew D <drewdowdy109@gmail.com>
2026-02-16 09:30:26 -0500

    completed 10_utf_16_string_values

--- js210/exercises/3_small_problems/10_utf_16_string_values.js
+++ js210/exercises/3_small_problems/10_utf_16_string_values.js
@@ -23,6 +23,8 @@ utf16Value('');                   // 0
 // The next three lines demonstrate that the code
 // works with non-ASCII characters from the UTF-16
 // character set.
+
 const OMEGA = "\u03A9";             // UTF-16 character 'Ω' (omega)
+
 utf16Value(OMEGA);                  // 937
 utf16Value(OMEGA + OMEGA + OMEGA);  // 2811
