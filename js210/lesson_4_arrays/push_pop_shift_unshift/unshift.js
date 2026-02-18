e000ce4c73f6a28f534f37b1968186ec40d9c7d0
Drew D <drewdowdy109@gmail.com>
2026-02-18 18:13:27 -0500

    completed unshift

---
+++ js210/lesson_4_arrays/push_pop_shift_unshift/unshift.js
@@ -0,0 +1,16 @@
+/*
+Write a function named unshift that accepts two arguments: an Array and a value. The function should insert the value at the beginning of the Array, and return the new length of the array. You will need a for loop for this problem.
+*/
+
+function unshift(array, item) {
+  for (let i = array.length; i > 0; i -= 1) {
+    array[i] = array[i - 1];
+  }
+
+  array[0] = item;
+  return array.length;
+}
+
+let count = [1, 2, 3];
+console.log(unshift(count, 0));      // 4
+console.log(count);                  // [ 0, 1, 2, 3 ]
