1ebb7e15c6d2ec9a33e16fa706f010e53c83cbf5
Drew D <drewdowdy109@gmail.com>
2026-02-18 11:35:39 -0500

    completed pop

---
+++ js210/lesson_4_arrays/push_pop_shift_unshift/pop.js
@@ -0,0 +1,13 @@
+/*
+Write a function named pop that accepts one argument: an Array. The function should remove the last element from the array and return it.
+*/
+
+function pop(array) {
+  let item = array[array.length - 1];
+  array.length = array.length - 1;
+  return item;
+}
+
+let count = [1, 2, 3];
+console.log(pop(count));             // 3
+console.log(count);                  // [ 1, 2 ]
