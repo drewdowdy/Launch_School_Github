d4a8f98b503237b5488fd5a1191231e774bd36b5
Drew D <drewdowdy109@gmail.com>
2026-02-19 15:17:34 -0500

    completed 6_lastNOf

---
+++ js210/lesson_4_arrays/basic_array_uses/6_lastNOf.js
@@ -0,0 +1,9 @@
+// Write a function like the previous one, except this time return the last count elements as a new array.
+
+function lastNOf(arr, count) {
+  let firstIndex = arr.length - count;
+  return arr.slice(firstIndex);
+}
+
+let digits = [4, 8, 15, 16, 23, 42];
+console.log(lastNOf(digits, 3));    // returns [16, 23, 42]
