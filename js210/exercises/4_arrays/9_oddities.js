2aa95f92d87516e5927e5fd2f56bae6325c70bcf
Drew D <drewdowdy109@gmail.com>
2026-02-24 08:43:13 -0500

    completed 9_oddities

---
+++ js210/exercises/4_arrays/9_oddities.js
@@ -0,0 +1,22 @@
+/*
+The oddities function takes an array as an argument and returns a new array containing every other element from the input array. The values in the returned array are the first (index 0), third, fifth, and so on, elements of the input array. The program below uses the array returned by oddities as part of a comparison. Can you explain the results of these comparisons?
+*/
+
+function oddities(array) {
+  const oddElements = [];
+
+  for (let i = 0; i < array.length; i += 2) {
+    oddElements.push(array[i]);
+  }
+
+  return oddElements;
+}
+
+oddities([2, 3, 4, 5, 6]) === [2, 4, 6];      // false
+oddities(['abc', 'def']) === ['abc'];         // false
+
+
+
+/*
+Both of these return false because strict equality `===` checks if two items are the same object. The result returned by `oddities` is a different array with the same value. No matter what we put, these will always evaluate to false.
+*/
