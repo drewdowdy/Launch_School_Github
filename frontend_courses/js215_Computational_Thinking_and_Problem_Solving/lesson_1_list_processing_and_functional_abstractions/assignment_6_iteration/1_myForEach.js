4e8e6d3d7a98ad8e49f317fbd5cd9b49178286c8
Drew D <drewdowdy109@gmail.com>
2026-04-29 09:05:53 -0400

    complted 1_myForEach

---
+++ frontend_courses/js215_Computational_Thinking_and_Problem_Solving/lesson_1_list_processing_and_functional_abstractions/assignment_6_iteration/1_myForEach.js
@@ -0,0 +1,13 @@
+// Write a Function named myForEach that is similar to the built-in Array.prototype.forEach method. Your Function should take an array and another Function as arguments. The Function passed to myForEach should reassign a variable in the outer scope. For instance, in the code fragment below, the Function passed to myForEach reassigns the min variable.
+
+function myForEach(array, func) {
+  for (let i = 0; i < array.length; i++) {
+    // pass the current element, index, and calling array
+    func(array[i], i, array); 
+  }
+}
+
+let min = Infinity;
+let getMin = value => (min = value <= min ? value : min);
+myForEach([4, 5, 12, 23, 3], getMin);
+console.log(min);                        // 3
