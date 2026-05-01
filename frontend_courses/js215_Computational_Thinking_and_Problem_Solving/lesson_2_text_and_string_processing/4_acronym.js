93bdf1ead3eaeb55a8f851c9797ab6f0a0279f4a
Drew D <drewdowdy109@gmail.com>
2026-05-01 16:58:48 -0400

    completed 4_acronym

---
+++ frontend_courses/js215_Computational_Thinking_and_Problem_Solving/lesson_2_text_and_string_processing/4_acronym.js
@@ -0,0 +1,14 @@
+// Write a function that generates and returns an acronym from a string of words. For example, the function should return "PNG" for the string "Portable Network Graphics". Count compound words (words connected with a dash) as separate words.
+
+function acronym(string) {
+  return string
+    .split(/[\s-]/g)
+    .map(w => w[0].toUpperCase())
+    .join('');
+}
+
+acronym('Portable Network Graphics');                  // "PNG"
+acronym('First In, First Out');                        // "FIFO"
+acronym('PHP: HyperText Preprocessor');                // "PHP"
+acronym('Complementary metal-oxide semiconductor');    // "CMOS"
+acronym('Hyper-text Markup Language');                 // "HTML"
