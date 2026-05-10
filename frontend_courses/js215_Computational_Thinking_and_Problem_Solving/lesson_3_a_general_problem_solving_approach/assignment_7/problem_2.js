835e571a02232b75f405a5b4c38363a2a99059ff
Drew D <drewdowdy109@gmail.com>
2026-05-10 19:22:08 -0400

    completed problem_2

---
+++ frontend_courses/js215_Computational_Thinking_and_Problem_Solving/lesson_3_a_general_problem_solving_approach/assignment_7/problem_2.js
@@ -0,0 +1,24 @@
+/*
+Given an array of integers, nums, return the third largest number in the array. If the third largest number does not exist, return the greatest number.
+
+You are not allowed to sort the array.
+
+-----
+PDBAD
+
+=== PROBLEM ===
+
+Write a function that given an array of numbers returns the 3rd largest number
+
+Rules:
+- if there is no 3rd largest number, return the largest number
+- cannot sort the array
+
+Questions:
+- will the input array always be non-empty?
+- will the input array always contain only numbers?
+- will the input array contain negative number?
+- will the array contain special Number characters (Infinity, -Infinity, NaN)?
+*/
+
+thirdMax([3, 2, 1]); // 1
