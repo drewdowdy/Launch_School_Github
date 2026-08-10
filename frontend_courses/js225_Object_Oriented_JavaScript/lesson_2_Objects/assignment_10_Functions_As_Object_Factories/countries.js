d28e4d854da146b39cd7c1f6a565c7edd6327d8b
Drew D <drewdowdy109@gmail.com>
2026-08-10 08:21:44 -0400

    created countries

---
+++ frontend_courses/js225_Object_Oriented_JavaScript/lesson_2_Objects/assignment_10_Functions_As_Object_Factories/countries.js
@@ -0,0 +1,21 @@
+function makeCountry(name, continent, visited=false) {
+  return {
+    name,
+    continent,
+    visited,
+    getDescription() {
+      console.log(this.name + ' is located in ' + this.continent + '.', `I have${this.visited ? '' : 'n\'t'} visited ${this.name}.`);
+    },
+    visitCountry() {
+      this.visited = true;
+    },
+  }
+}
+
+let chile = makeCountry('The Republic of Chile', 'South America');
+let canada = makeCountry('Canada', 'North America');
+let southAfrica = makeCountry('The Republic of South Africa', 'Africa');
+
+chile.getDescription();       // "The Republic of Chile is located in South America."
+canada.getDescription();      // "Canada is located in North America."
+southAfrica.getDescription(); // "The Republic of South Africa is located in Africa."
