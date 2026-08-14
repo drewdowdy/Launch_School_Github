f14f8a8e80434eaaf64e37fee68ba9d974717a59
Drew D <drewdowdy109@gmail.com>
2026-08-13 22:43:32 -0400

    commented out wrong code

--- frontend_courses/js225_Object_Oriented_JavaScript/lesson_4_Closures_and_Function_Scope/assignment_16_IIFE/practice_problems/1.js
+++ frontend_courses/js225_Object_Oriented_JavaScript/lesson_4_Closures_and_Function_Scope/assignment_16_IIFE/practice_problems/1.js
@@ -1,8 +1,8 @@
 // Will the code below execute?
 
-function() {
-  console.log("Sometimes, syntax isn't intuitive!")
-}();
+// function() {
+//   console.log("Sometimes, syntax isn't intuitive!")
+// }();
 
 // Answer:
 // It will throw an error because you can't put invocation parenthesis directly after a funciton declaration
